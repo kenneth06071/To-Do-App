3577f3a0ed2f86a645a767c9cb56362c02ad06d9
kenneth06071 <kennethchung028@gmail.com>
2025-10-06 00:46:19 +0800

    added input box and submit button

--- source_code/src/Component/Todo.jsx
+++ source_code/src/Component/Todo.jsx
@@ -1,12 +1,20 @@
 import {useState} from "react"
+import Todoitem from "./Todoitem";  
 
 export default function Todo() {
     const [task, setTask] = useState("");
+    const [tasks, setTasks] = useState([]);
+    function handlesubmit(e){
+        e.preventDefault();
+        setTasks([...tasks, task])
+
+    }
     return( 
     <div>
-        <form>
+        <form onSubmit = {handlesubmit}>
             <input onChange = {(e)=>setTask(e.target.value)} type= "text" value = {task} />
             <button>Add</button>
+            {tasks.map(tasks => <Todoitem key = {tasks} item={tasks} />)}
         </form>
 
     </div>
